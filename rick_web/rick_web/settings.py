372683917ce213afd8bc7adc59b05033ee646b42
Ubuntu <ubuntu@ip-172-26-8-158.us-west-2.compute.internal>
2017-07-11 09:39:18 +0000

    post grammar fixes

--- rick_web/rick_web/settings.py
+++ rick_web/rick_web/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-#STATIC_URL  = '/static/'
-STATIC_URL = os.path.join(BASE_DIR,"static/")
+STATIC_URL  = '/static/'
+#STATIC_URL = os.path.join(BASE_DIR,"/www/static/")
 STATIC_ROOT = os.path.join(BASE_DIR,"../www/static/")
 
